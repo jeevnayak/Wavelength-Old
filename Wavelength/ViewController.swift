b88395adb8d92ff7999b080637aa2e3ab28d3734
Rajeev Nayak <jeev.nayak@gmail.com>
2014-12-12 21:57:47 -0800

    initial commit

---
+++ Wavelength/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wavelength
+//
+//  Created by Rajeev Nayak on 12/12/14.
+//  Copyright (c) 2014 jeev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
